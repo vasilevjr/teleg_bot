c79192188596b6354d15de8d5e629be73e26b563
vasilevjr <vasilevdv31@gmail.com>
2023-05-30 22:20:12 +0500

    dice game

--- handlers/dice_games.py
+++ handlers/dice_games.py
@@ -1,2 +1,44 @@
+from aiogram import types, Dispatcher, Bot, executor
+from config import bot, dp, ADMINS
+from asyncio import sleep
 
 
+async def dice_game2(message: types.Message):
+    if message.chat.type == "private" and message.from_user.id in ADMINS:
+        await bot.send_message(message.from_user.id, f"Саламчик, "
+                                                 f"{message.from_user.full_name}\n"
+                                                 f"крч, ща в кости сыграем")
+        await sleep(2)
+    else:
+        await message.answer("Ты не админ, тебе нельзя ):")
+
+
+
+    await bot.send_message(message.from_user.id, "Бот кидает!")
+    bot_data = await bot.send_dice(message.from_user.id)
+    bot_data = bot_data["dice"]["value"]
+    await sleep(5)
+
+
+    await bot.send_message(message.from_user.id, "Кидаешь ты!")
+    user_data = await bot.send_dice(message.from_user.id)
+    user_data = user_data["dice"]["value"]
+    await sleep(5)
+
+    if bot_data > user_data:
+        await bot.send_message(message.from_user.id, f"{message.from_user.full_name}\nТы проеал!")
+    elif bot_data < user_data:
+        await bot.send_message(message.from_user.id, f"{message.from_user.full_name}\nНе ссы,ты выеал!")
+    else:
+        await bot.send_message(message.from_user.id, "Нечьи не бывает, всё уйня давай по новой!")
+
+
+
+
+
+
+
+
+def register_handlers_dice_game(dp: Dispatcher):
+    dp.register_message_handler(dice_game2, commands=["dice"])
+    #регистрируем функции
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from aiogram import executor
 import logging
 
 from config import dp, bot, ADMINS
-from handlers import commands, callback, admin, extra, FSM_Admin_clients
+from handlers import commands, callback, admin, extra, FSM_Admin_clients, dice_games
 from database.bot_db import sql_create
 
 async def on_startup(db):
@@ -18,6 +18,7 @@ commands.register_message_commands(dp)
 callback.register_handlers_callback(dp)
 admin.register_handlers_admin(dp)
 FSM_Admin_clients.register_handlers_FSM_Admin_clients(dp)
+dice_games.register_handlers_dice_game(dp)
 
 extra.register_handlers_extra(dp)
 
